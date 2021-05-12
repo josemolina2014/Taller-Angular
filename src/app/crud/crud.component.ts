14fb2f5a81cb2380385ddb638d65db3a4dcffb86
jlmolina <j.l.molina@cgiar.org>
2021-05-11 21:47:41 -0500

    funciones de actualizar

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -9,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class CrudComponent implements OnInit {
 
   dtOptions: DataTables.Settings = {}; //configuracion de la tabla
-  
+
   // objeto con las personas que se mostraran en la tabla
   people = [
     {name: 'Ana', city: 'Cali', email: 'ana@outlook.com'},
@@ -21,8 +21,11 @@ export class CrudComponent implements OnInit {
   Name='';
   Email='';
 
+  posicion =-1;
+
+
   constructor() { }
-  
+
 
   ngOnInit(): void {
 
@@ -36,16 +39,42 @@ export class CrudComponent implements OnInit {
 
   }
 
+  resetForm(){
+    this.City = '';
+    this.Name='';
+    this.Email='';
+    this.posicion =-1;
+  }
+
  //agregar un registro
   Add(Name,Email,City){
     console.log('agregando datos...');
     var data = {name: Name, city : City, email:Email}
-    this.people.push(data);
+    if(this.posicion ==-1)
+    {
+      this.people.push(data);
+    }
+    else
+    {
+      this.people[this.posicion]=data;
+    }
+
+    this.resetForm();
+
   }
 
   //eliminar un registro
   delete(i){
     this.people.splice(i, 1);
+  }
+
+  edit(person)
+  {
+      this.posicion = this.people.indexOf(person);
+
+      this.City= person.city;
+      this.Name= person.name;
+      this.Email= person.email;
   }
 
 
